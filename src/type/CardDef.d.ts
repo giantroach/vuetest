dedc7c393aace6e4ef0ed911a11f21473d172482
Tomoki Motohashi <tomoki.motohashi@gmail.com>
2022-06-12 01:54:23 +0900

    add real card data

--- src/type/CardDef.d.ts
+++ src/type/CardDef.d.ts
@@ -10,14 +10,31 @@ interface TextLayoutDef {
   padding: string;
 }
 
-type Conditions = "handSelectable" | "targetAfterPlay" | "gridSelectable";
+type OnPlay =
+  | "TargetSameLane:Silence"
+  | "TargetNonStealthSameLane:Reincanate"
+  | "TargeAnytStealth:Reveal"
+  | "TargetSameLaneToAnother:Maze";
+type OnResolution =
+  | "IncleaseCenterBy1"
+  | "Receive1OnLose"
+  | "Becomes15OnCenter0or6"
+  | "ChangeCenterTo0"
+  | "LowerWins"
+  | "TiesOn4+"
+  | "Deal1OnWin"
+  | "ReducesCenterBy2";
 
 interface CardDetail {
+  name: string;
   text: string;
-  conditions?: any;
-  // conditions?: {
-  //   [key: Conditions]: Function;
-  // };
+  power: {
+    fixed: number;
+    center: 0 | 1 | 2 | 3;
+  };
+  onPlay?: OnPlay;
+  onResolution?: OnResolution;
+  stealth: boolean;
 }
 
 interface CardDef {
